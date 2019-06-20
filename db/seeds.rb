746e8cb673ad750b69bfd0e37bc61b414c782bf4
sasasoni <chibichibi1997@hotmail.co.jp>
2019-06-20 15:53:00 +0900

    Add item seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+items = %w(食費 趣味 本 その他)
+
+items.each do |item|
+  Item.create!(
+    name: item
+  )
+end
