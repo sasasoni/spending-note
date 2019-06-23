f9388c8774e6ce0af03e949186e12d9244d56997
sasasoni <chibichibi1997@hotmail.co.jp>
2019-06-24 04:09:26 +0900

    Add login fliendly forwarding

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :store_current_location, unless: :devise_controller?
 
   private
 
@@ -11,5 +12,10 @@ class ApplicationController < ActionController::Base
 
   def login_required
     redirect_to new_user_session_path, notice: "ログインしてください" unless current_user
+  end
+
+  def store_current_location
+    return if current_user
+    store_location_for(:user, request.url)
   end
 end
--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -2,6 +2,7 @@
 
 class Users::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
+  before_action :reset_session_before_login, only: :create
 
   # GET /resource/sign_in
   # def new
@@ -24,4 +25,13 @@ class Users::SessionsController < Devise::SessionsController
   # def configure_sign_in_params
   #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
   # end
+
+  private
+
+  def reset_session_before_login
+    user_return_to = session[:user_return_to]
+    reset_session
+
+    session[:user_return_to] = user_return_to if user_return_to
+  end
 end
