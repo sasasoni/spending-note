59cc79d1724998b0cc8239a6d9b4eff633677749
sasasoni <chibichibi1997@hotmail.co.jp>
2019-06-23 19:45:10 +0900

    Fix Item seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,10 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-items = %w(食費 趣味 本 その他)
+items = %w(食費 日用品費 交際費 趣味 医療費 交通費 その他)
 
 items.each do |item|
   Item.create!(
-    item_name: item
+    name: item
   )
 end
