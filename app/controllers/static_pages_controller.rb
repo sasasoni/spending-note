65cd1a7beda68dee6f029264d7ae5a7c5f01385a
sasasoni <chibichibi1997@hotmail.co.jp>
2019-06-23 21:20:04 +0900

    Add contact and help page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,4 +2,10 @@ class StaticPagesController < ApplicationController
   def home
     redirect_to survey_costs_path if current_user
   end
+
+  def help
+  end
+
+  def contact
+  end
 end
--- app/models/cost.rb
+++ app/models/cost.rb
@@ -13,9 +13,12 @@ class Cost < ApplicationRecord
   scope :latest, -> { order(created_at: :desc) }
   scope :period, -> (date) do
     # start_date = 先月の25日00:00:00
-    start_date = date.last_month.beginning_of_month.advance(days: 24)
+    # start_date = date.last_month.beginning_of_month.advance(days: 24)
     # end_date = 今月の24日23:59:59
-    end_date = date.beginning_of_month.advance(days: 23).end_of_day
+    # end_date = date.beginning_of_month.advance(days: 23).end_of_day
+
+    start_date = date.beginning_of_month
+    end_date = date.end_of_month
 
     # 支払い日が指定した期間内にあるものを取り出す
     where("paid_date >= ? AND paid_date  <= ?", start_date, end_date)
