a6fba7c960dc55be045f676c0dcf8a4e5cf90818
sasasoni <chibichibi1997@hotmail.co.jp>
2019-06-30 01:44:42 +0900

    remove demand record method

--- app/models/demand.rb
+++ app/models/demand.rb
@@ -5,10 +5,10 @@ class Demand < ApplicationRecord
     { only_integer: true, greater_than_or_equal_to: 0 }
   validates :memo, length: { maximum: 200 }
 
-  def self.record(user)
-    total_cost = user.costs.take_demands(user).sum(:expenditure)
-    user.demands.create(total_cost: total_cost, approved: false, received: false)
-  end
+  # def self.record(user)
+  #   total_cost = user.costs.take_demands(user).sum(:expenditure)
+  #   user.demands.create(total_cost: total_cost, approved: false, received: false)
+  # end
 
   def approve
     update_columns(approved: true)
